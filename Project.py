e34301c4f89b9b5dba08093c43f3c8d7c153579a
Mohamad Fawaz <mohaf2001@gmail.com>
2024-07-12 20:51:36 +0300

    I added the the linked lists logic to the script to be able to use it later

--- Project.py
+++ Project.py
@@ -14,3 +14,67 @@ def employee():
             pass
         elif entry == 3:
             pass
+class Node:
+    def __init__(self,info,next):
+        self.info = info
+        self.next = next
+
+class Linked_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def addtohead(self,info):
+        n = Node(info,None)
+        if self.size == 0:
+            self.head = n
+            self.tail = n
+            self.size = 1
+        else:
+            n.head = self.head
+            self.head = n
+            self.size += 1
+
+    def addtotail(self,info):
+        n = Node(info,None)
+        if self.size == 0:
+            return self.addtohead(info)
+        else:
+            self.tail.next = n
+            self.tail = n
+            self.size += 1
+
+    def deletehead(self):
+        if self.size == 0:
+            return
+        elif self.size == 1:
+            val = self.head.info
+            self.head = None
+            self.tail = None
+            self.size = 0
+            return val
+        else:
+            val = self.head.info
+            self.head = self.head.next
+            self.size -=1
+            return val
+
+    def deletetail(self):
+        if self.size <= 1:
+            return self.deletehead
+        else:
+            val = self.tail.info
+            i = self.head
+            while i.next.next != None:
+                i = i.next
+            self.tail = i
+            self.tail.next = None
+            return val
+
+    def printLL(self,info):
+        i = self.head
+        while i != None:
+            print(i.info,"->",end = "")
+            i = i.next
+        print()
