2b6cbe9b8d8c63638c83e59abe75611498c3b79c
Mohamad Fawaz <mohaf2001@gmail.com>
2024-07-12 18:21:59 +0300

    started with defining a function that communicates with the employee at the company with if-elif statements that take the employee to the other menus

---
+++ Project.py
@@ -0,0 +1,16 @@
+def employee():
+    s = '''
+    Hello! Please enter:
+    1. To go to the drivers' menu
+    2. To go to the cities' menu
+    3. To exit the system
+    '''
+    print(s)
+    entry = int(input("Enter one of the options(1, 2 or 3): "))
+    while entry == 1 or entry == 2 or entry == 3:
+        if entry == 1:
+            pass
+        elif entry == 2:
+            pass
+        elif entry == 3:
+            pass
